4609dab8ec3f33f9065a6a2eeeecf07e98f78c4a
pje8845 <pje991025@gmail.com>
2020-06-05 04:31:30 +0900

    ssu_crontab.c 명령어 추가 삭제 구현 완료!

--- ssu_crontab(주기적명령어실행프로그램)/ssu_crontab.c
+++ ssu_crontab(주기적명령어실행프로그램)/ssu_crontab.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 #include <fcntl.h>
 #include <string.h>
 #include <signal.h>
@@ -37,7 +38,7 @@ int main() {
             fprintf(stderr, "fopen error for %s\n", crontabLog);
             exit(1);
         }
-        close(fp);
+        fclose(fp);
     }
     //프롬프트 실행
     if(!doPrompt())
@@ -46,33 +47,35 @@ int main() {
 }
 
 int printPrompt(void) {
-    char *prompt = " \n20180753> ";
+    char *prompt = "\n 20180753> ";
     char listbuf[BUFLEN], idxbuf[BUFLEN];
     int idx = 0;
     char *ptr;
     if (!access(crontabFile, F_OK)) { //파일이 이미 존재하는 경우
         if ((cronfp = fopen(crontabFile, "r")) == NULL) {
             fprintf(stderr, "fopen error for %s\n", crontabFile);
-            return FALSE;
+            return -1;
         }
         while(fgets(listbuf, BUFLEN, cronfp) != NULL) { //저장된 모든 명령어 출력
             sprintf(idxbuf, "%d. ", idx++);
-            puts(idxbuf); //현재 명령어 번호 출력
-            if (puts(listbuf) == EOF) { //뿌려주기
+            fputs(idxbuf,stdout); //현재 명령어 번호 출력
+            if (fputs(listbuf, stdout) == EOF) { //뿌려주기
                 fprintf(stderr, "puts error\n");
-                return FALSE;
+                return -1;
             }
         }
         fclose(cronfp);
-    } 
+        fputs(prompt, stdout);
+        return idx;
+    }
     //파일 없는 경우 제어 바로 여기로 옴
+    printf("파일없음\n");
     fputs(prompt, stdout); //프롬프트 출력
     return idx;
 }
 
 int doPrompt(void) {
-    char *prompt = " \n20180753> ";
-    char listbuf[BUFLEN], idxbuf[BUFLEN], logbuf[BUFLEN], timebuf[BUFLEN];
+    char errorbuf[BUFLEN], logbuf[BUFLEN], timebuf[BUFLEN];
     int idx = 0, i;
     char *ptr;
     Schedule schedule;
@@ -86,12 +89,14 @@ int doPrompt(void) {
             memset(argv[i], (char)0, BUFLEN);
         }
 
-        if ((idx = printPrompt()) == 0) { //프롬프트 출력
+        if ((idx = printPrompt()) < 0) { //프롬프트 출력
             fprintf(stderr, "printPrompt error!\n");
             return FALSE;
         }
-        while (fgets(Command, BUFLEN, stdin) == NULL) { //엔터만 받으면 다시 재입력
+        fgets(Command, BUFLEN, stdin);
+        while (!strcmp(Command, "\n")) { //엔터만 받으면 다시 재입력
             printPrompt();
+            fgets(Command, BUFLEN, stdin);
         }
         if (ferror(stdin)) { //stdin에러처리
             fprintf(stderr, "standard input error\n");
@@ -107,7 +112,6 @@ int doPrompt(void) {
 
         memset(usrCommand, 0,BUFLEN);
         if(strstr(argv[0], "add") != NULL) {
-            printf("--add 명령어--\n");
             //ssu_crontab_file 여부 확인
             if (access(crontabFile, F_OK)) { //파일이 없는 경우
                 if ((cronfp = fopen(crontabFile, "w+")) == NULL) { //새로 생성
@@ -122,8 +126,10 @@ int doPrompt(void) {
                 }
             }
             
-            if (!checkSchedule()) //실행주기 올바르게 입력되었는지 확인
+            if (!checkSchedule()) { //실행주기 올바르게 입력되었는지 확인
                 continue; //안된경우 프롬프트 출력
+                
+            }
             else { //실행주기 확인 완료!
                 for(i=1; i<6; i++) { //argv[1]~argv[5]까지 저장
                     strcat(usrCommand, argv[i]);
@@ -155,7 +161,7 @@ int doPrompt(void) {
             }
         }
         else if(strstr(argv[0], "remove") != NULL) {
-            printf("--remove 명령어--\n");
+            //printf("--remove 명령어--\n");
             if (argc != 2) { //명령어 인자 개수 확인!
                 fprintf(stderr, "usage : remove <REMOVE NUMBER>\n");
                 continue;
@@ -166,14 +172,14 @@ int doPrompt(void) {
                 continue;
             }
             strcat(usrCommand, argv[0]);
-            strcat(usrCommand, argv[1]);
+            //strcat(usrCommand, argv[1]);
             if (!doRemove(rmvnum)) //삭제 실패
                 continue;
             else { //정상적으로 삭제된 경우 -> 로그파일에 기록
                 time(&cur_time);
 	            ctime_r(&cur_time, timebuf);
                 timebuf[19] = 0; //요일 월 일 시간 까지만 출력할것임
-                sprintf(logbuf, "[%s] %s\n", timebuf, usrCommand);
+                sprintf(logbuf, "[%s] %s\n", timebuf, usrCommand); //해당 명령어 
                 if((logfp = fopen(crontabLog, "a+")) < 0){
                     fprintf(stderr, "fopen error for %s\n", crontabLog);
                     return FALSE;
@@ -186,20 +192,45 @@ int doPrompt(void) {
             }
         }
         else if (strstr(argv[0], "exit") != NULL) {
-            printf("--exit 명령어--\n");
+            //printf("--exit 명령어--\n");
             break;
         }
         else { //이외의 명령어 -> 프롬프트 출력'
-            fprintf(stderr, "<%s> command not exist!", argv[1]);
+            snprintf(errorbuf, strlen(argv[0]), "%s", argv[0]);
+            fprintf(stderr, "<%s> command not exist!\n", errorbuf);
             continue;
         }
     }
     return TRUE;
 }
 
-int doAdd(char *command) {
-    //파라미터로 넘어온 명령어를 ssu_crontab_file 파일에 추가한다.
+char *rtrim(char *str) { //문자열의 우측에 존재하는 공백을 제거 후 리턴
+	char tmp[BUFLEN];
+	char *end;
 
+	strcpy(tmp, str);
+	end = tmp + strlen(tmp) - 1;
+	while (end != str && isspace(*end))
+		--end;
+
+	*(end + 1) = '\0';
+	str = tmp;
+	return str;
+}
+
+int doAdd(char *command) { //파라미터로 넘어온 명령어를 ssu_crontab_file 파일에 추가한다.
+    FILE *fp;
+    if ((fp = fopen(crontabFile, "a")) == NULL) {
+        fprintf(stderr, "fopen error for %s\n", crontabFile);
+        return FALSE;
+    }
+    command[strlen(command)-1] = '\0';
+    //sprintf(command, "%s\n", command);
+    if (fputs(command, fp) < 0) { //내용 추가
+        fprintf(stderr, "fupts error\n");
+        return FALSE;
+    }
+    fclose(fp);
 }
 
 int doRemove(int num) {
@@ -224,14 +255,14 @@ int doRemove(int num) {
             idx++;
             continue;
         }
-        if (fputs(listbuf, tmpfp) == NULL) {
+        if (fputs(listbuf, tmpfp) < 0) {
             fprintf(stderr, "fputs error\n");
             return FALSE;
         }
         idx++;
     }
     fclose(cronfp);
-    fclose(tmpfile);
+    fclose(tmpfp);
     if (remove(crontabFile) < 0) { //원본파일 삭제
         fprintf(stderr, "remove error\n");
         return FALSE;
@@ -243,30 +274,152 @@ int doRemove(int num) {
     return TRUE; 
 }
 
-int checkToken(Schedule schedule) {
-    //실행 주기가 제대로 입력되었는지 확인 후, 올바르면 TRUE 틀리면 FALSE 리턴
-    int i;
-    int min;
-   
+int checkRange(int i, int num) {
+    switch (i) {
+        case 1: //분 항목의 범위 확인
+        //printf("case 1:");
+            if (num < 0 || num > 59) {
+                    //fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            else
+                break;
+        case 2 : //시 항목의 범위 확인
+            if (num < 0 || num > 23) {
+                    //fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            else
+                break;
+        case 3 : //일 항목의 범위 확인
+            if (num < 1 || num > 31) {
+                    //fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            else
+                break;
+        case 4 : //월 항목의 범위 확인
+            if (num < 1 || num > 12) {
+                    //fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            else
+                break;
+        case 5 : //요일 항목의 범위 확인
+            if (num < 0 || num > 6) {
+                    //fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            else
+                break;
+    }
+    return TRUE;
 }
 
 int checkSchedule(void) {
     char *ptr;
     int i = 0;
-    Schedule schedule;
+    int num=0, n1 =0, n2=0;
+
     if (argc < 7) 
         return FALSE;
-    strcpy(schedule.min, argv[1]);
-    strcpy(schedule.hour,argv[2]);
-    strcpy(schedule.day, argv[3]);
-    strcpy(schedule.mon, argv[4]);
-    strcpy(schedule.dayofweek, argv[5]);
-    if (!checkToken(schedule))
-        return FALSE;
-    else {
-        printf("Schedule : %s %s %s %s %s\n", schedule.min, schedule.hour, schedule.day, schedule.mon, schedule.dayofweek);
-        return TRUE;
+    
+    for (i=1; i<6; i++) {
+        //printf("argv[%d]:%s길이 : %ld\n", i, argv[i],strlen(argv[i]));
+        if ((strlen(argv[i]) == 1) || (strlen(argv[i]) == 2)) { //해당 항목의 길이가 1이나 2인 경우
+            if(!strcmp(argv[i], "*")) { //'*'만 있어야 함
+                continue;
+            }
+            else if (isdigit(argv[i][0])) { //숫자만 있어야 함
+                //숫자인 경우 범위 확인 필요
+                num = atoi(argv[i]);
+                if (!checkRange(i, num)) { //항복의 숫자 범위 확인
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                if ((strlen(argv[i]) == 2) && num < 10) { //길이가 2인데 숫자 한자리면 오류
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+            }
+            else {
+                printf("숫자도 별도 아님\n");
+                fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            continue;
+        }
+        else { //길이가 3 이상, 1미만인 경우
+            if (strstr(argv[i], "-") != NULL) { //범위 지정인 경우 : 숫자-숫자 구조여야 함
+                ptr = strtok(argv[i], "-");
+                if (!isdigit(ptr)) {
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                n1 = atoi(ptr);
+                if (!checkRange(i, n1)) {
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                ptr = strtok(NULL, "-"); //하이픈 다음 숫자 가져옴
+                if (!isdigit(ptr)) {
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                n2 = atoi(ptr);
+                if (!checkRange(i, n2)) {
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                if (n1 > n2 || n1 == n2) { //숫자1-숫자2 구조중 두 숫자가 같거나, 앞 숫자가 더 큰경우
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+            }
+            else if (strstr(argv[i], "*/") != NULL) { //건너뛰는 경우1 : */숫자 구조여야 함
+                ptr = strtok(argv[i], "*/");
+                if (!isdigit(ptr[0])) { //문자 뒤 내용이 숫자가 아니면 오류
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                num = atoi(ptr);
+                if (!checkRange(i, num)) {
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+            }
+            else if (strstr(argv[i], ",") != NULL) { // 목록의 경우 : 모든 숫자가 범위 안에 포함되어야 함
+                ptr = strtok(argv[i], ",");
+                while (ptr != NULL) { //목록의 경우 모든 숫자가 범위에 속하는지 확인
+                    num = atoi(ptr);
+                    if (!checkRange(i, num)) { //항복의 숫자 범위 확인
+                        fprintf(stderr, "실행주기 입력 오류\n");
+                        return FALSE;
+                    }
+                    ptr = strtok(NULL, ",");
+                }
+            }
+            else if ((strstr(argv[i], "*/") == NULL) && (strstr(argv[i], "/") != NULL)) { //건너뛰는 경우2 : 숫자/숫자 구조여야 함 (-과 같이 쓰일 수 있으므로 if문으로)
+                ptr = strtok(argv[i], "/");
+                if (isdigit(ptr[0])) { //숫자인지 확인
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+                ptr = strtok(NULL, "/"); 
+                if (isdigit(ptr[0])) { // '/'다음 문자도 숫자인지 확인
+                    fprintf(stderr, "실행주기 입력 오류\n");
+                    return FALSE;
+                }
+            }
+            else { //그 외의 문자가 사용된 경우
+                fprintf(stderr, "실행주기 입력 오류\n");
+                return FALSE;
+            }
+            
+            
+        }
     }
+    return TRUE;
 }
 
 
