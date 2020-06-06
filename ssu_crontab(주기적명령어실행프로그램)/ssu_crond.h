d17ae94907b1751e0a4b0f37ef26905508e25cf8
pje8845 <pje991025@gmail.com>
2020-06-06 23:03:14 +0900

    crond, crontab 완성git add .

--- ssu_crontab(주기적명령어실행프로그램)/ssu_crond.h
+++ ssu_crontab(주기적명령어실행프로그램)/ssu_crond.h
@@ -10,3 +10,12 @@
 #ifndef BUFLEN
 	#define BUFLEN 1024
 #endif
+
+char crontabFile[BUFLEN]; //주기적으로 실행할 명령어가 들어가있는 파일 이름
+char crontabLog[BUFLEN]; //명령어의 변경사항이나, 정상 수행된 경우 해당 내용을 기록할 로그 파일 이름
+char workDir[BUFLEN]; //현재 작업디렉토리의 경로를 저장해줌
+
+int crond_daemon_init(void);
+int doCrond(void);
+int checkTime(char *time, int type);
+int compareTime(char *time, int type);
--- ssu_crontab(주기적명령어실행프로그램)/ssu_crontab.h
+++ ssu_crontab(주기적명령어실행프로그램)/ssu_crontab.h
@@ -1,5 +1,3 @@
-#include <pthread.h>
-//crontab이 사용할 헤더
 #ifndef TRUE
 	#define TRUE 1
 #endif
@@ -15,20 +13,14 @@
 #define COMMAND_ARGC 100
 
 char crontabFile[BUFLEN]; //주기적으로 실행할 명령어가 들어가있는 파일 이름
-crontabLog[BUFLEN]; //명령어의 변경사항이나, 정상 수행된 경우 해당 내용을 기록할 로그 파일 이름
+char crontabLog[BUFLEN]; //명령어의 변경사항이나, 정상 수행된 경우 해당 내용을 기록할 로그 파일 이름
+char workDir[BUFLEN]; //현재 작업디렉토리의 경로를 저장해줌
 
-pthread_cond_t cond1 = PTHREAD_COND_INITIALIZER;
-pthread_cond_t cond2 = PTHREAD_COND_INITIALIZER;
-
-typedef struct schedule {
-    char min[8];
-    char hour[8];
-    char day[8];
-    char mon[8];
-    char dayofweek[8];
-} Schedule;
-
-int doPrompt(void);
-int checkSchedule(void);
-int crond_daemon_init(void);
 int printPrompt(void);
+int doPrompt(void);
+char *rtrim(char *str);
+int doRemove(int num);
+int doAdd(char *command);
+int checkRange(int num, int type);
+int checkItem(char *item, int type);
+int checkSchedule(void);
